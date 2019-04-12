8e47028d2f1b5b4a08d378bd5b494d8bb9acc780
Benjamin DUBUS <benjamin.dubus@epitech.eu>
2019-04-12 12:37:18 -0400

    2 route ready

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 # Import flask and template operators
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
+import requests
 
 # Could import flask extensions, such as SQLAlchemy, here
 # from flask.ext.sqlalchemy import SQLAlchemy
 
 # Define WSGI object
 app = Flask(__name__)
+app.config['TESTING'] = True
 
 # Configurations
 app.config.from_object('config')
@@ -20,12 +22,23 @@ app.config.from_object('config')
 def not_found(error):
     return render_template('404.html'), 404
 
+
+@app.route("/name", methods=['POST'])
+def name():
+    print("http://127.0.0.1:5000/api/v1/search/" + request.form['name'])
+    r = requests.get("http://127.0.0.1:5000/api/v1/search/" + request.form['name'])
+    result = r.json()
+    if (result['error'] == True):
+        return "Erreur"
+    return render_template("result.html", result=result)
+
+@app.route("/random", methods=['POST'])
+def random():
+    r = requests.get("http://127.0.0.1:5000/api/v1/random")
+    result = r.json()
+    return render_template("result.html", result=result)
+
 # Home page view
-@app.route('/')
+@app.route('/',  methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
-
-
-# Build the database:
-# This will init the db
-# db.init_app(app)
